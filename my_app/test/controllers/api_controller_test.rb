77609bd88259bee5b5538c00f2f6e9a64d9dc086
eday1632 <ericday87@gmail.com>
2019-11-08 18:12:42 -0800

    editing tests

--- my_app/test/controllers/api_controller_test.rb
+++ my_app/test/controllers/api_controller_test.rb
@@ -6,25 +6,18 @@ class ApiControllerTest < ActionDispatch::IntegrationTest
         assert_response(200)
     end
 
+    test 'get a request' do
+        assert_routing '/api/2', controller: 'api', action: 'show', id: '2'
+    end
+
     test 'request not found' do
         get api_path id: '10'
         assert_response(404)
     end
 
-    test 'post a comment' do
-        get api_index_url
-
-        post api_index_url id: '1', params: { request: { id: '1', comment: 'can create' } }
-        assert_response(200)
-    end
-
     test 'put method not allowed' do
         put api_path id: '1', params: { request: { id: '1', comment: 'can create' } }
         assert_response(405)
-    end
-
-    test 'get a request' do
-        assert_routing '/api/2', controller: 'api', action: 'show', id: '2'
     end
 
 end
