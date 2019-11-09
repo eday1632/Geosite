aeb31ad948021c31f06c2685fb6cc8704b6781b2
eday1632 <ericday87@gmail.com>
2019-11-08 18:04:14 -0800

    added functionality, tests

--- my_app/app/controllers/api_controller.rb
+++ my_app/app/controllers/api_controller.rb
@@ -1,48 +1,56 @@
 class ApiController < ApplicationController
+    rescue_from ActiveRecord::RecordNotFound, :with => :record_not_found
+
     def index
-        curr_req = Request.new()
-        curr_req.req_type = request.method
+        curr_req = Request.new(:req_type => request.method)
         curr_req.save
 
         @requests = Request.select(:created_at).order(created_at: :desc).limit(10)
 
         @date = current_date
         @cpuinfo = cpu_info
-
-        @test_req = request.env
-
     end
 
     def show
         @request = Request.find(params[:id])
     end
 
-    def new
-    end
-
-    def edit
-    end
-
     def create
+        @request = Request.find(params[:id])
+        @request.update(request_params)
     end
 
     def update
+        render :text => '405 Method Not Allowed', :status => 405
     end
 
     def destroy
+        @request = Request.find(params[:id])
+        @request.destroy
     end
 
     private
+        def request_params
+            params.require(:request).permit(:comment)
+        end
+
         def current_date
-            # t = Thread.new { exec('date') }
-            # t.join
+            # tbd: tradeoffs between blocking, new thread, new proc
+            # super easily crashed?
             `date`
         end
 
         def cpu_info
-            # Thread.new { exec('cat /proc/cpuinfo') }
+            # tbd: tradeoffs between blocking, new thread, new proc
+
             # %x|cat /proc/cpuinfo|
             %x|cat /Users/ericday/Documents/junk|
         end
+
+        # copied func from tutorial
+        def record_not_found
+            render :text => "404 Not Found", :status => 404
+        end
+
 end
 
--- my_app/test/controllers/api_controller_test.rb
+++ my_app/test/controllers/api_controller_test.rb
@@ -1,9 +1,30 @@
 require 'test_helper'
 
 class ApiControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get api_index_url
-    assert_response :success
-  end
+    test 'should get index' do
+        get api_index_url
+        assert_response(200)
+    end
+
+    test 'request not found' do
+        get api_path id: '10'
+        assert_response(404)
+    end
+
+    test 'post a comment' do
+        get api_index_url
+
+        post api_index_url id: '1', params: { request: { id: '1', comment: 'can create' } }
+        assert_response(200)
+    end
+
+    test 'put method not allowed' do
+        put api_path id: '1', params: { request: { id: '1', comment: 'can create' } }
+        assert_response(405)
+    end
+
+    test 'get a request' do
+        assert_routing '/api/2', controller: 'api', action: 'show', id: '2'
+    end
 
 end
